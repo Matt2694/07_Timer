fdbab29d5748b2ce876817d5e17dbbd7a5d7c71f
Matthew Anthony Peterson <matt2694@edu.eal.dk>
2016-09-20 11:44:15 +0200

    Formatting

--- 07_Timer/Timer.cs
+++ 07_Timer/Timer.cs
@@ -1,32 +1,30 @@
 ﻿using System;
 
-namespace _07_Timer
-{
-    internal class Timer
-    {
+namespace _07_Timer{
+
+    internal class Timer{
+
         private double seconds = 0;
         private double hours = 0;
         private double minutes = 0;
         private double newSeconds = 0;
-        public double Seconds
-        {
-            get
-            {
+        public double Seconds{
+
+            get{
                 return seconds;
             }
-            internal set
-            {
+            internal set{
                 seconds = value;
                 SecondsToHMS();
             }
         }
-        public override string ToString()
-        {
+        public override string ToString(){
+
             string format = string.Format("{0:00}:{1:00}:{2:00}", hours, minutes, newSeconds);
             return format;
         }
-        public void SecondsToHMS()
-        {
+        public void SecondsToHMS(){
+
             double holding;
             double holding2;
             newSeconds = Seconds % 60;
@@ -36,8 +34,7 @@ namespace _07_Timer
             hours = minutes - holding2;
             minutes = holding2;
             hours = hours / 60;
-            if (hours < 1)
-            {
+            if (hours < 1){
                 hours = 0;
             }
         }
