fd6e687ff13cc3515ba0cdfd62e5ed433ba3ddba
Matthew Anthony Peterson <matt2694@edu.eal.dk>
2016-09-20 09:42:36 +0200

    Add project files.

---
+++ 07_Timer/Timer.cs
@@ -0,0 +1,28 @@
+﻿namespace _07_Timer
+{
+    internal class Timer
+    {
+        private double seconds;
+        private double hours;
+        private double minutes;
+        private double newSeconds;
+        public double Seconds {
+            get
+            {
+                return seconds;
+            }
+            internal set
+            {
+                seconds = value;
+            }
+        }
+        public override string ToString()
+        {
+            
+        }
+        public void SecondsToHMS()
+        {
+
+        }
+    }
+}
