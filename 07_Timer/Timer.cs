7d41a57d0d2901ecd13349dd675a4669d4117852
Matthew Anthony Peterson <matt2694@edu.eal.dk>
2016-09-20 11:42:17 +0200

    Completed program

--- 07_Timer/Timer.cs
+++ 07_Timer/Timer.cs
@@ -1,12 +1,15 @@
-﻿namespace _07_Timer
+﻿using System;
+
+namespace _07_Timer
 {
     internal class Timer
     {
-        private double seconds;
-        private double hours;
-        private double minutes;
-        private double newSeconds;
-        public double Seconds {
+        private double seconds = 0;
+        private double hours = 0;
+        private double minutes = 0;
+        private double newSeconds = 0;
+        public double Seconds
+        {
             get
             {
                 return seconds;
@@ -14,15 +17,29 @@
             internal set
             {
                 seconds = value;
+                SecondsToHMS();
             }
         }
         public override string ToString()
         {
-            
+            string format = string.Format("{0:00}:{1:00}:{2:00}", hours, minutes, newSeconds);
+            return format;
         }
         public void SecondsToHMS()
         {
-
+            double holding;
+            double holding2;
+            newSeconds = Seconds % 60;
+            holding = Seconds - newSeconds;
+            minutes = holding / 60;
+            holding2 = minutes % 60;
+            hours = minutes - holding2;
+            minutes = holding2;
+            hours = hours / 60;
+            if (hours < 1)
+            {
+                hours = 0;
+            }
         }
     }
 }
